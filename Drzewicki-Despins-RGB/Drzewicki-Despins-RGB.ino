b7cd7fe77ed460c3778b171fabd565003f8d3b5d
Dylan Despins <02dylan.despins@gulllakecs.org>
2019-05-02 12:26:37 -0400

    Void Loop Code

--- Drzewicki-Despins-RGB/Drzewicki-Despins-RGB.ino
+++ Drzewicki-Despins-RGB/Drzewicki-Despins-RGB.ino
@@ -21,11 +21,12 @@ void setup() {
   lcd.print("hello, world!");
 }
 
-void loop() {
+void loop(){
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
   lcd.print(millis() / 1000);
 }
+ 
  
