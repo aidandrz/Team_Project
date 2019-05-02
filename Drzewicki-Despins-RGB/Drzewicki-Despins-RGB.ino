623f13a6507ce1ac6b5fdfe422e94566e4dafcff
Dylan Despins <02dylan.despins@gulllakecs.org>
2019-05-02 12:24:20 -0400

    Void Setup Code

--- Drzewicki-Despins-RGB/Drzewicki-Despins-RGB.ino
+++ Drzewicki-Despins-RGB/Drzewicki-Despins-RGB.ino
@@ -1,18 +1,31 @@
 /*
  * 5.1.19
  * Creators:
- * Aidan Drzewicki and Dylan Despins
- * We are going to make an RGB LED 
+ * Aidan Drzewicki and Dylan Despins 
+ * We are going to make LCD screen
+ * Public Domain 
  */
 
 
 
-void setup() {
-  // put your setup code here, to run once:
+// include the library code:
+#include <LiquidCrystal.h>
 
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
 }
+ 
