c1f90f380e644e9a91eb60b8295c8e21f4aeb063
Dylan Despins <02dylan.despins@gulllakecs.org>
2019-05-20 12:29:56 -0400

    Initial Commits

--- Drzewicki-Despins-RGB/Drzewicki-Despins-RGB.ino
+++ Drzewicki-Despins-RGB/Drzewicki-Despins-RGB.ino
@@ -2,28 +2,13 @@
  * 5.1.19
  * Creators:
  * Aidan Drzewicki and Dylan Despins 
- * We are going to make LCD screen
+ * We are going to make a light sensor with a photoresistor
  * Public Domain 
- */
+*/
 
+int greenLedPin = 2;
+int yellowLedPin = 3;
+int redLedPin = 4;
 
-
-// include the library code:
-#include <LiquidCrystal.h>
-
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
-void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello world!");
-}
-
-void loop(){
-  lcd.home();
-  lcd.print("tentacle");
-}
- 
- 
+int lightSensorPin = A0;
+int analogValue = 0;
